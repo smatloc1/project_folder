4acfa4429c7b239fd2c4801296b109bfecb66307
smatloc1 <optimizequality@gmail.com>
2021-04-08 11:56:59 +0000

    corrected tables

--- model.py
+++ model.py
@@ -9,51 +9,59 @@ db = SQLAlchemy()
 class Org(db.Model):
     """ Organization Profile """
 
-        __tablename__ = "orgs"
+    __tablename__ = "orgs"
 
-        org_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-        org_name = db.Column(db.String(50), nullable=False, unique=True)
-        #org_type = db.Column(db.String(2), default='NP')
-        org_vision = db.Column(db.String(100))
-        org_mission = db.Column(db.String(100))
-        #mission_cat = db.Column(db.String(3), Foreign_key=True) 
-        #ntsee_code = db.Column(db.String(3)), nullable=True)
-        #project_type = db.Column(db.String(50))
-        contact = db.Column(db.String(50))
-        exec_sum = db.Column(db.String(50))
-        #region = db.Column(db.Integer,nullable=False)
-        address = db.Column(db.String(50),nullable=False )     
-        city = db.Column(db.String(20), nullable=False)
-        state = db.Column(db.String(2), nullable=False)
+    org_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    org_name = db.Column(db.String(50), nullable=False, unique=True)
+    org_type = db.Column(db.String(2), default='NP')
+    org_vision = db.Column(db.String(100))
+    org_mission = db.Column(db.String(100))
+    mission_cat = db.Column(db.String(3), foreign_key=True) 
+    ntsee_code = db.Column(db.String(3), nullable=True)
+    project_type = db.Column(db.String(50))
+    contact = db.Column(db.String(50))
+    exec_sum = db.Column(db.String(50))
+    region = db.Column(db.String, nullable=True)
+    address = db.Column(db.String(50),nullable=False )   
+    city = db.Column(db.String(20), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
         
 
     def __repr__(self):
-        return
+        return f'<Org org_id = {self.org_id}, org_name = {self.org_name}>'
 
 class Mission(db.Model):
     """ Organization Missions """
 
-        __tablename__ = "missions"
+    __tablename__ = "missions"
 
-        mission_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-        mission_cat = db.Column(db.String(50), nullable=False, Foreign_key=True)
-        ntsee_code = db.Column(db.String(3))
+    mission_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    mission_cat = db.Column(db.String(50), nullable=False, foreign_key=True)
+    ntsee_code = db.Column(db.String(3))
     
-  db.create_all()
+    def __repr__(self):
+        return f'<Mission mission_id = {self.mission_id}, mission_cat = {self.mission_cat}>'
+  
+ 
 
 ##########################   SETUP SQLALCHEMY   ###############################
 
 
-def connect_to_db(app, orgs):   
-    """ Connect to database """
 
-    app config['SQLALCHEMY_DATABASE_URI'] = 'posgresql:///orgs
-    app config['SQLALCHEMY_ECHO'] = True
+def connect_to_db(app, db_name='postgresql:///orgs', echo=True):
+    """Connect to database """
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_name
+    app.config['SQLALCHEMY_ECHO'] = echo
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     db.app = app
     db.init_app(app)
 
-connect_to_db(app, orgs)
+    print('connected to db')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    from server import app
+    
+
+    db.create_all()
